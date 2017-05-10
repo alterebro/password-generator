1d5f8559b743db18ba6bbd8f3c6aa44cf59a76a7
Jorge Moreno <alterebro@gmail.com>
2017-05-10 12:40:25 +0200

    cleanup redundant code

--- src/password-generator.js
+++ src/password-generator.js
@@ -2,9 +2,8 @@
 	'use strict';
 
 	var passgen = function(pass, site, size) {
-		var bases = window.Bases;
 		var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\"";
-		var pass = bases.toAlphabet( bases.fromBase16((md5(md5(pass) + md5(site)))) , alphabet );
+		var pass = Bases.toAlphabet( Bases.fromBase16((md5(md5(pass) + md5(site)))) , alphabet );
 		return pass.slice(0, size);
 	}
 
