e7ad1b29689f312c786d9d04801d81e4e225fac0
Jorge Moreno <alterebro@gmail.com>
2017-05-11 11:40:43 +0200

    small generated output fix

--- src/password-generator.js
+++ src/password-generator.js
@@ -4,7 +4,8 @@
 	var passgen = function(pass, site, size) {
 		var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\"";
 		var pass = Bases.toAlphabet( Bases.fromBase16((md5(md5(pass) + md5(site)))) , alphabet );
-		return pass.slice(0, size);
+			pass = Array(20).join(pass);
+		return pass.slice(0, parseInt(size));
 	}
 
 	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
